da2571c2fb7a57cb912f0b85f9a9472dc6de2be0
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:36:01 +0530

    Add files via upload

---
+++ Project 4/Main.js
@@ -0,0 +1,14 @@
+// User Data Entry System 
+var serial_No = prompt("Enter your serial no : "); 
+var name = prompt("Enter your name : "); 
+var age = prompt("Enter your age : "); 
+var address = prompt("Enter your address"); 
+var designation = prompt("Enter your Designation"); 
+var salary = prompt("Enter your salary : ");
+document.write("User Data Entry System\n"); 
+document.write(`Serial-No : ${serial_No}`); 
+document.write(`Name : ${name} \n`); 
+document.write(`Age : ${age} \n`); 
+document.write(`Address : ${address} \n`); 
+document.write(`Designation : ${designation} \n`); 
+document.write(`Salary : ${salary} \n`); 
