fcb3a93b88dd18cb9c2a6ff1d9d2d2728ce245c6
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:38:43 +0530

    Add files via upload

---
+++ Project 7/main.js
@@ -0,0 +1,57 @@
+// 1. Odd Even Checker Programm :  
+// var readline = require("readline"); 
+// var rl = readline.createInterface({
+//     input:process.stdin, 
+//     output:process.stdout
+// }); 
+// rl.question("Enter a number : ", function(num){
+//     if(num%2 == 0){
+//         console.log( `${num}` + " " + "Number is 'Even' "); 
+//     } else{
+//         console.log(`${num}` + " " + "Number is 'Odd' ");  
+//     }
+//     rl.close(); 
+// }); 
+// Even : 2, 4, 6, 8, 10, 12 
+// Odd : 1, 3, 5, 7, 9, 11, 13, 15
+
+
+// 2. Positive and Negative Number Checker : 
+// var readline = require("readline"); 
+// var rl = readline.createInterface({
+//     input:process.stdin, 
+//     output:process.stdout
+// }); 
+// rl.question("Enter a number : ", function(num){
+//     if(num > 0){
+//         console.log( `${num}` + " " + "Number is 'Positive' "); 
+//     } 
+//     else if(num < 0){
+//         console.log( `${num}` + " " + "Number is 'Negative' "); 
+//     } 
+//      else{
+//         console.log(`${num}` + " " + "Not a valid number");  
+//     }
+//     rl.close(); 
+// }); 
+
+
+
+// 3. Prime number finder programm ? 
+//   var num = parseInt(prompt("Enter a number : ")); 
+//         var isPrime = true; 
+//         for(var i = 2; i < num; i++){
+//             if(num%i == 0){
+//                 isPrime = false; 
+//                 break;
+//             }
+//           }
+//         if(isPrime){
+//             console.log("Number is prime"); 
+//         } else{
+//             console.log("Not a prime"); 
+//         }
+      
+
+// Prime Numbers : 2, 3, 5, 7, 11, 13, 17, 19, 23, 27, 29.........  
+
